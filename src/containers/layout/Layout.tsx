4971a3814666af5e94c6240abf2b6271c4b21b96
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-24 21:47:07 +0430

    Create layout file

---
+++ src/containers/layout/Layout.tsx
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const Layout = () => {
+  return <Text>This is Layout section</Text>;
+};
+
+export default Layout;
