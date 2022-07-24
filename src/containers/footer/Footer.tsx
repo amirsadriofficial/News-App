78cae30e04d3a002a485c98c48428293dcf7901b
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-24 21:44:57 +0430

    Create footer file

---
+++ src/containers/footer/Footer.tsx
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const Footer = () => {
+  return <Text>This is Footer section</Text>;
+};
+
+export default Footer;
