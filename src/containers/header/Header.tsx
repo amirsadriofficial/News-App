d34265d7d6cedc0eaa01a8445784aded7f7aef6a
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-24 21:46:06 +0430

    Create header file

---
+++ src/containers/header/Header.tsx
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+
+const Header = () => {
+  return <Text>This is Header section</Text>;
+};
+
+export default Header;
