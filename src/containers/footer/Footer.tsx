226ab3993670eee53c63370f8f7452166a6e0dee
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-27 12:52:05 +0430

    Add all icon

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -3,15 +3,22 @@
  */
 
 import React from 'react';
-import {View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Footer = () => {
   return (
-    <View>
-      <Feather name="home" size={50} color="#000" />
+    <SafeAreaView>
+      <Octicons name="home" size={50} color="#000" />
       <Feather name="search" size={50} color="#000" />
-    </View>
+      <Ionicons name="notifications-outline" size={50} color="#000" />
+      <FontAwesome name="bookmark-o" size={50} color="#000" />
+      <AntDesign name="user" size={50} color="#000" />
+    </SafeAreaView>
   );
 };
 
