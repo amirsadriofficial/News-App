fcab6f3a1a7b6067b78d619786567019019674c5
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-27 12:58:45 +0430

    Add title for icons

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import Octicons from 'react-native-vector-icons/Octicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -13,11 +13,26 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 const Footer = () => {
   return (
     <SafeAreaView>
-      <Octicons name="home" size={50} color="#000" />
-      <Feather name="search" size={50} color="#000" />
-      <FontAwesome name="bookmark-o" size={50} color="#000" />
-      <Ionicons name="notifications-outline" size={50} color="#000" />
-      <AntDesign name="user" size={50} color="#000" />
+      <View>
+        <Octicons name="home" size={50} color="#000" />
+        <Text>Home</Text>
+      </View>
+      <View>
+        <Feather name="search" size={50} color="#000" />
+        <Text>Search</Text>
+      </View>
+      <View>
+        <FontAwesome name="bookmark-o" size={50} color="#000" />
+        <Text>Saved</Text>
+      </View>
+      <View>
+        <Ionicons name="notifications-outline" size={50} color="#000" />
+        <Text>Notifications</Text>
+      </View>
+      <View>
+        <AntDesign name="user" size={50} color="#000" />
+        <Text>Profile</Text>
+      </View>
     </SafeAreaView>
   );
 };
