123bad6e275ceafaed8eed78e3ff81c046c734a6
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-27 10:14:47 +0430

    Add icon

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -9,8 +9,8 @@ import Feather from 'react-native-vector-icons/Feather';
 const Footer = () => {
   return (
     <View>
-      <Feather name="home" size={50} color="#ccc" />
-      <Feather name="search" size={50} color="#ccc" />
+      <Feather name="home" size={50} color="#000" />
+      <Feather name="search" size={50} color="#000" />
     </View>
   );
 };
