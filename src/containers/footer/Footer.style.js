28907259ae0a258d01601240db8991565e66acab
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-27 13:07:49 +0430

    Add styles file to footer

---
+++ src/containers/footer/Footer.style.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginHorizontal: 8,
+    // borderRadius: 10,
+    // marginBottom: 8,
+    // paddingHorizontal: 16,
+  },
+  footerItems: {
+    // display: 'flex',
+    // flexDirection: 'column',
+    // marginHorizontal: 8,
+    // borderRadius: 10,
+    // marginBottom: 8,
+    // paddingHorizontal: 16,
+  },
+});
--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -9,27 +9,28 @@ import Octicons from 'react-native-vector-icons/Octicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import styles from './Footer.style'
 
 const Footer = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.footer}>
+      <View style={styles.footer}>
         <Octicons name="home" size={50} color="#000" />
         <Text>Home</Text>
       </View>
-      <View>
+      <View style={styles.footer}>
         <Feather name="search" size={50} color="#000" />
         <Text>Search</Text>
       </View>
-      <View>
+      <View style={styles.footer}>
         <FontAwesome name="bookmark-o" size={50} color="#000" />
         <Text>Saved</Text>
       </View>
-      <View>
+      <View style={styles.footer}>
         <Ionicons name="notifications-outline" size={50} color="#000" />
         <Text>Notifications</Text>
       </View>
-      <View>
+      <View style={styles.footer}>
         <AntDesign name="user" size={50} color="#000" />
         <Text>Profile</Text>
       </View>
