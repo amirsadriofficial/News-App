ab763c099b3cb8e285ebd14a3be12410f046b576
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-30 10:46:45 +0430

    Update packages

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -2,19 +2,53 @@
  * @format
  */
 
-import React from 'react';
-import {Text} from 'react-native';
-import Header from './Header';
-import Footer from '../../containers/footer/Footer';
+import * as React from 'react';
+import {View, useWindowDimensions} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
 
-const Home = () => {
+const FirstRoute = () => <View style={{flex: 1, backgroundColor: '#ff4081'}} />;
+
+const SecondRoute = () => (
+  <View style={{flex: 1, backgroundColor: '#673ab7'}} />
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+export default function TabViewExample() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: 'first', title: 'First'},
+    {key: 'second', title: 'Second'},
+  ]);
+
   return (
-    <>
-      <Header />
-      <Text>This is Home page</Text>
-      <Footer />
-    </>
+    <TabView
+      navigationState={{index, routes}}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{width: layout.width}}
+    />
   );
-};
+}
 
-export default Home;
+// import React from 'react';
+// import {Text} from 'react-native';
+// import Header from './Header';
+// import Footer from '../../containers/footer/Footer';
+
+// const Home = () => {
+//   return (
+//     <>
+//       <Header />
+//       <Text>This is Home page</Text>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Home;
