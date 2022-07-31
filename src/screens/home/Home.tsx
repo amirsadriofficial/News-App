90abf9d15705628d77558cda5f32840b5f4f6431
Amir Sadri <amirsadriofficial@gmail.com>
2022-07-31 17:41:55 +0430

    Implement top tabs in home page

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -3,17 +3,77 @@
  */
 
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Layout from '../../containers/layout/Layout';
 import Header from './Header';
+
+const ChatScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>ChatScreen</Text>
+    </View>
+  );
+};
+const StatutScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>StatutScreen</Text>
+    </View>
+  );
+};
+const CallScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>CallScreen</Text>
+    </View>
+  );
+};
+
+const Tab = createMaterialTopTabNavigator();
 
 const Home = ({navigation}: any) => {
   return (
     <Layout navigation={navigation}>
       <Header />
       <Text>This is Home page</Text>
+      {/* <NavigationContainer> */}
+        <Tab.Navigator>
+          <Tab.Screen name="Chats" component={ChatScreen} />
+          <Tab.Screen name="Status" component={StatutScreen} />
+          <Tab.Screen name="Calls" component={CallScreen} />
+        </Tab.Navigator>
+        <StatusBar />
+      {/* </NavigationContainer> */}
     </Layout>
   );
 };
 
 export default Home;
+
+// const App = () => {
+//   return (
+// <NavigationContainer>
+//   <Tab.Navigator>
+//     <Tab.Screen name="Chats" component={ChatScreen} />
+//     <Tab.Screen name="Status" component={StatutScreen} />
+//     <Tab.Screen name="Calls" component={CallScreen} />
+//   </Tab.Navigator>
+//   <StatusBar />
+// </NavigationContainer>
+//   );
+// };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+// export default App;
