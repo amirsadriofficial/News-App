e3f804d5330039e8fd613823928f6221e1e46782
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-01 13:11:24 +0430

    Create popular carousel file

---
+++ src/screens/home/PopularCarousel.tsx
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+// import styles from './styles/Header.style';
+
+const PopularCarousel = () => {
+  return (
+    <View>
+      <Text>Popular Posts</Text>
+    </View>
+  );
+};
+
+export default PopularCarousel;
