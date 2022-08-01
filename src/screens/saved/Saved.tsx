1e99817eed8942427627d4ec02d34f919d6b348e
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-01 19:40:23 +0430

    Change code of saved file

---
+++ src/screens/saved/Saved.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Layout from '../../containers/layout/Layout';
+
+const Saved = ({navigation}: any) => {
+  return (
+    <Layout navigation={navigation}>
+      <Text>This is Saved page</Text>
+    </Layout>
+  );
+};
+
+export default Saved;
