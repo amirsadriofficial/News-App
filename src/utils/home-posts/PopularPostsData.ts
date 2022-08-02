88718b8d3954fef6852934e96c1ce0fca169e0d7
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-02 23:29:25 +0430

    Create popular category data

--- src/utils/home-posts/PopularPostsData.ts
+++ src/utils/home-posts/PopularPostsData.ts
@@ -32,3 +32,78 @@ export const PopularSportsData = [
     date: '24 dbd 1953',
   },
 ];
+
+export const PopularScientificData = [
+  {
+    id: '1',
+    image: PoliticsPost3,
+    title: 'Firddhst Item',
+    date: '24 fbd 2022',
+  },
+  {
+    id: '2',
+    image: PoliticsPost4,
+    title: 'twdnhbdsnhbo Item',
+    date: '24 dbd 1953',
+  },
+];
+
+export const PopularTechnologyData = [
+  {
+    id: '1',
+    image: PoliticsPost3,
+    title: 'Firddhst Item',
+    date: '24 fbd 2022',
+  },
+  {
+    id: '2',
+    image: PoliticsPost4,
+    title: 'twdnhbdsnhbo Item',
+    date: '24 dbd 1953',
+  },
+];
+
+export const PopularEconomicalData = [
+  {
+    id: '1',
+    image: PoliticsPost3,
+    title: 'Firddhst Item',
+    date: '24 fbd 2022',
+  },
+  {
+    id: '2',
+    image: PoliticsPost4,
+    title: 'twdnhbdsnhbo Item',
+    date: '24 dbd 1953',
+  },
+];
+
+export const PopularSocialData = [
+  {
+    id: '1',
+    image: PoliticsPost3,
+    title: 'Firddhst Item',
+    date: '24 fbd 2022',
+  },
+  {
+    id: '2',
+    image: PoliticsPost4,
+    title: 'twdnhbdsnhbo Item',
+    date: '24 dbd 1953',
+  },
+];
+
+export const PopularEducationalData = [
+  {
+    id: '1',
+    image: PoliticsPost3,
+    title: 'Firddhst Item',
+    date: '24 fbd 2022',
+  },
+  {
+    id: '2',
+    image: PoliticsPost4,
+    title: 'twdnhbdsnhbo Item',
+    date: '24 dbd 1953',
+  },
+];
