db23d53459c53e9bce5f858b159aa49684b4a64b
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-02 20:13:12 +0430

    Add popular sports data

--- src/utils/home-posts/PopularPostsData.ts
+++ src/utils/home-posts/PopularPostsData.ts
@@ -1,5 +1,7 @@
 import PoliticsPost1 from './../../assets/posts/1.png';
-import PoliticsPost2 from './../../assets/posts/3.png';
+import PoliticsPost2 from './../../assets/posts/2.png';
+import PoliticsPost3 from './../../assets/posts/3.png';
+import PoliticsPost4 from './../../assets/posts/4.png';
 
 export const PopularPoliticsData = [
   {
@@ -13,5 +15,20 @@ export const PopularPoliticsData = [
     image: PoliticsPost2,
     title: 'two Item',
     date: '24 augous 1953',
+  },
+];
+
+export const PopularSportsData = [
+  {
+    id: '1',
+    image: PoliticsPost3,
+    title: 'Firddhst Item',
+    date: '24 fbd 2022',
+  },
+  {
+    id: '2',
+    image: PoliticsPost4,
+    title: 'twdnhbdsnhbo Item',
+    date: '24 dbd 1953',
   },
 ];
