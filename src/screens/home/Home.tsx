b0a7b7db056f1dcb061b5de50401335ffdf6ff96
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-02 20:07:49 +0430

    Create styles file of home screen

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -3,33 +3,34 @@
  */
 
 import React from 'react';
-import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Layout from '../../containers/layout/Layout';
 import Header from './Header';
-import PopularCarousel from '../../components/popular-posts-slider/PopularPostsSlider';
+import PopularPostsSlider from '../../components/popular-posts-slider/PopularPostsSlider';
+import {PopularPoliticsData} from '../../utils/home-posts/PopularPostsData';
+// import styles from './styles/Home.style';
 
-const ChatScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>ChatScreen</Text>
-    </View>
-  );
+const PopularPoliticsSlider = () => {
+  return <PopularPostsSlider data={PopularPoliticsData} />;
 };
-const StatutScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>StatutScreen</Text>
-    </View>
-  );
-};
-const CallScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>CallScreen</Text>
-    </View>
-  );
-};
+// const PopularPoliticsSlider = () => {
+//   return <PopularPostsSlider data={PopularPoliticsData} />;
+// };
+// const PopularPoliticsSlider = () => {
+//   return <PopularPostsSlider data={PopularPoliticsData} />;
+// };
+// const PopularPoliticsSlider = () => {
+//   return <PopularPostsSlider data={PopularPoliticsData} />;
+// };
+// const PopularPoliticsSlider = () => {
+//   return <PopularPostsSlider data={PopularPoliticsData} />;
+// };
+// const PopularPoliticsSlider = () => {
+//   return <PopularPostsSlider data={PopularPoliticsData} />;
+// };
+// const PopularPoliticsSlider = () => {
+//   return <PopularPostsSlider data={PopularPoliticsData} />;
+// };
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -42,28 +43,17 @@ const Home = ({navigation}: any) => {
           tabBarScrollEnabled: true,
           tabBarItemStyle: {width: 115},
         }}>
-        <Tab.Screen name="Politics" component={PopularCarousel} />
-        <Tab.Screen name="Sport" component={StatutScreen} />
-        <Tab.Screen name="Scientific" component={CallScreen} />
-        <Tab.Screen name="Technology" component={ChatScreen} />
-        <Tab.Screen name="Economical" component={CallScreen} />
-        <Tab.Screen name="Social" component={CallScreen} />
-        <Tab.Screen name="Educational" component={CallScreen} />
+        <Tab.Screen name="Politics" component={PopularPoliticsSlider} />
+        <Tab.Screen name="Sport" component={PopularPoliticsSlider} />
+        <Tab.Screen name="Scientific" component={PopularPoliticsSlider} />
+        <Tab.Screen name="Technology" component={PopularPoliticsSlider} />
+        <Tab.Screen name="Economical" component={PopularPoliticsSlider} />
+        <Tab.Screen name="Social" component={PopularPoliticsSlider} />
+        <Tab.Screen name="Educational" component={PopularPoliticsSlider} />
       </Tab.Navigator>
-      <StatusBar />
+      {/* <StatusBar /> */}
     </Layout>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-  },
-});
 
 export default Home;
