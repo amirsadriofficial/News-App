f627224835285db613d54f1e5dab0ad3547b8248
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-02 19:38:25 +0430

    Create popular post data file

---
+++ src/utils/home-posts/PopularPostsData.ts
@@ -0,0 +1,14 @@
+export const PopularPoliticsData = [
+  {
+    id: '1',
+    image: Picture1,
+    title: 'First Item',
+    date: '24 june 2022',
+  },
+  {
+    id: '2',
+    image: Picture2,
+    title: 'two Item',
+    date: '24 augous 1953',
+  },
+];
