427b2fd7c8144ed55e32278cc6e2021fd1d533e1
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-02 17:22:44 +0430

    Change code of new posts file

---
+++ src/components/new-posts-slider/NewPostsSlider.tsx
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const NewPostsSlider = () => {
+  return (
+    <View>
+      <Text>Amir Sadri</Text>
+    </View>
+  );
+};
+
+export default NewPostsSlider;
