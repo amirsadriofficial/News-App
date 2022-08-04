df5a15f98e41aff3c880774b0c85c1e20c3baa79
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-04 15:54:36 +0430

    Add new image to popular post

--- src/utils/home-posts/PopularPostsData.ts
+++ src/utils/home-posts/PopularPostsData.ts
@@ -2,6 +2,10 @@ import PoliticsPost1 from './../../assets/posts/1.png';
 import PoliticsPost2 from './../../assets/posts/2.png';
 import PoliticsPost3 from './../../assets/posts/3.png';
 import PoliticsPost4 from './../../assets/posts/4.png';
+import PoliticsPost5 from './../../assets/posts/5.png';
+import PoliticsPost6 from './../../assets/posts/6.png';
+import PoliticsPost7 from './../../assets/posts/7.png';
+import PoliticsPost8 from './../../assets/posts/8.png';
 
 export const PopularPoliticsData = [
   {
@@ -36,14 +40,14 @@ export const PopularSportsData = [
 export const PopularScientificData = [
   {
     id: '1',
-    image: PoliticsPost3,
-    title: 'Firddhst Item',
+    image: PoliticsPost5,
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 fbd 2022',
   },
   {
     id: '2',
-    image: PoliticsPost4,
-    title: 'twdnhbdsnhbo Item',
+    image: PoliticsPost6,
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 dbd 1953',
   },
 ];
@@ -51,14 +55,14 @@ export const PopularScientificData = [
 export const PopularTechnologyData = [
   {
     id: '1',
-    image: PoliticsPost3,
-    title: 'Firddhst Item',
+    image: PoliticsPost7,
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 fbd 2022',
   },
   {
     id: '2',
-    image: PoliticsPost4,
-    title: 'twdnhbdsnhbo Item',
+    image: PoliticsPost8,
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 dbd 1953',
   },
 ];
@@ -67,13 +71,13 @@ export const PopularEconomicalData = [
   {
     id: '1',
     image: PoliticsPost3,
-    title: 'Firddhst Item',
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 fbd 2022',
   },
   {
     id: '2',
     image: PoliticsPost4,
-    title: 'twdnhbdsnhbo Item',
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 dbd 1953',
   },
 ];
@@ -82,13 +86,13 @@ export const PopularSocialData = [
   {
     id: '1',
     image: PoliticsPost3,
-    title: 'Firddhst Item',
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 fbd 2022',
   },
   {
     id: '2',
     image: PoliticsPost4,
-    title: 'twdnhbdsnhbo Item',
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 dbd 1953',
   },
 ];
@@ -97,13 +101,13 @@ export const PopularEducationalData = [
   {
     id: '1',
     image: PoliticsPost3,
-    title: 'Firddhst Item',
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 fbd 2022',
   },
   {
     id: '2',
     image: PoliticsPost4,
-    title: 'twdnhbdsnhbo Item',
+    title: 'Tracking the States Where Abortion Is Now Banned',
     date: '24 dbd 1953',
   },
 ];
