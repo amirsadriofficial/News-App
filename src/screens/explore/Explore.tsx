b6e6856278d7b7bfa08240ff0d7636eeaa296e93
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-04 19:27:51 +0430

    Create and styling explore header

--- src/screens/explore/Explore.tsx
+++ src/screens/explore/Explore.tsx
@@ -3,13 +3,28 @@
  */
 
 import React from 'react';
-import {Text} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 import Layout from '../../containers/layout/Layout';
+import styles from './styles/Explore.style';
 
 const Explore = ({navigation}: any) => {
+  const [number, onChangeNumber] = React.useState('');
+
   return (
     <Layout navigation={navigation}>
-      <Text>This is Explore page</Text>
+      <SafeAreaView>
+        <View style={styles.inputContainer}>
+          <Feather name="search" size={25} color="#000" />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Search..."
+            keyboardType="default"
+          />
+        </View>
+      </SafeAreaView>
     </Layout>
   );
 };
---
+++ src/screens/explore/styles/Explore.style.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: 16,
+    paddingHorizontal: 16,
+    backgroundColor: '#fff',
+    borderRadius: 32,
+  },
+  input: {
+    marginLeft: 8,
+    height: '100%',
+    width: '100%',
+  },
+});
