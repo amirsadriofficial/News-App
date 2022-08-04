84d0055fe4ccc0e7a19f15c610b19a681d9adb13
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-04 22:13:46 +0430

    Complete implement explore page

--- src/components/explore-category/ExploreCategory.tsx
+++ src/components/explore-category/ExploreCategory.tsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import {View, Text, Image, ScrollView} from 'react-native';
+import {View, Text, Image, ScrollView, TouchableOpacity} from 'react-native';
 import styles from './styles/ExploreCategory.style';
 import ExploreCategoryData from '../../utils/explore-posts/ExploreCategoryData';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -13,10 +13,12 @@ const ExploreCategory = () => {
     <ScrollView>
       <SafeAreaView style={styles.container}>
         {ExploreCategoryData.map(category => (
-          <View style={styles.category}>
-            <Image source={category.image} style={styles.image} />
-            <Text style={styles.title}>{category.title}</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={styles.category}>
+              <Image source={category.image} style={styles.image} />
+              <Text style={styles.title}>{category.title}</Text>
+            </View>
+          </TouchableOpacity>
         ))}
       </SafeAreaView>
     </ScrollView>
