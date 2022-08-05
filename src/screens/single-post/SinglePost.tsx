18b4c85935d177f7ab5dcc2f4b37734abdb97413
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 22:26:23 +0430

    Implement single post header

--- src/screens/single-post/SinglePost.tsx
+++ src/screens/single-post/SinglePost.tsx
@@ -4,19 +4,32 @@
 
 import React from 'react';
 import {Text, View, Image} from 'react-native';
-import Layout from '../../containers/layout/Layout';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Octicons from 'react-native-vector-icons/Octicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import SavedPostData from '../../utils/saved-posts/SavedPostData';
+import styles from './styles/SinglePost.style';
 
 const SinglePost = ({route, navigation}: any) => {
   const {id} = route.params;
   const post = SavedPostData.filter(obj => obj.id === id);
   return (
-    <Layout navigation={navigation}>
-      <View>
-        <Image source={post[0].image} />
-        <Text>{post[0].title}</Text>
+    <View>
+      <View style={styles.header}>
+        <AntDesign name="arrowleft" size={30} color="#1DA1F2" />
+        <View style={styles.rightHeader}>
+          <Octicons
+            name="share-android"
+            size={30}
+            color="#1DA1F2"
+            style={styles.shareIcon}
+          />
+          <FontAwesome name="bookmark-o" size={30} color="#1DA1F2" />
+        </View>
       </View>
-    </Layout>
+      <Image source={post[0].image} style={styles.image} />
+      <Text>{post[0].title}</Text>
+    </View>
   );
 };
 
--- src/screens/single-post/styles/SinglePost.style.ts
+++ src/screens/single-post/styles/SinglePost.style.ts
@@ -1,7 +1,27 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  header: {},
+  header: {
+    backgroundColor: '#fff',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  },
+  rightHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  shareIcon: {
+    marginRight: 24,
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
 });
 
 export default styles;
