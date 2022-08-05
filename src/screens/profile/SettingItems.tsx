c462858a52f2a33d049818e5c3fa4992a5b50f63
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 15:46:42 +0430

    Implement profile screen

--- src/screens/profile/SettingItems.tsx
+++ src/screens/profile/SettingItems.tsx
@@ -3,14 +3,59 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
-// import styles from './styles/SettingItems.style';
+import {Text, View, SafeAreaView} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import styles from './styles/SettingItems.style';
 
 const SettingItems = () => {
   return (
-    <View>
-      <Text>Setting Items</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.item}>
+        <View style={styles.titleSection}>
+          <AntDesign name="user" size={30} color="#1DA1F2" />
+          <Text style={styles.title}>Personal Data</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={30} color="#1DA1F2" />
+      </View>
+      <View style={styles.item}>
+        <View style={styles.titleSection}>
+          <Ionicons name="settings-outline" size={30} color="#1DA1F2" />
+          <Text style={styles.title}>Setting</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={30} color="#1DA1F2" />
+      </View>
+      <View style={styles.item}>
+        <View style={styles.titleSection}>
+          <AntDesign name="download" size={30} color="#1DA1F2" />
+          <Text style={styles.title}>Downloads</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={30} color="#1DA1F2" />
+      </View>
+      <View style={styles.item}>
+        <View style={styles.titleSection}>
+          <Feather name="help-circle" size={32} color="#1DA1F2" />
+          <Text style={styles.title}>Help</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={30} color="#1DA1F2" />
+      </View>
+      <View style={styles.item}>
+        <View style={styles.titleSection}>
+          <AntDesign name="infocirlceo" size={30} color="#1DA1F2" />
+          <Text style={styles.title}>About</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={30} color="#1DA1F2" />
+      </View>
+      <View style={styles.item}>
+        <View style={styles.titleSection}>
+          <AntDesign name="logout" size={30} color="#1DA1F2" />
+          <Text style={styles.title}>Log out</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={30} color="#1DA1F2" />
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/profile/styles/SettingItems.style.ts
+++ src/screens/profile/styles/SettingItems.style.ts
@@ -1,5 +1,31 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    borderRadius: 40,
+    marginTop: 16,
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '90%',
+    marginLeft: 18,
+  },
+  titleSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 16,
+    paddingVertical: 16,
+  },
+  title: {
+    fontSize: 20,
+    marginLeft: 8,
+    color: '#282828',
+  },
+});
 
 export default styles;
