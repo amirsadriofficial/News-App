4308b2a53552e4da97c1c5c660e3085ef6fbd783
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 09:54:54 +0430

    Create saved post component

--- src/components/saved-post/SavedPost.tsx
+++ src/components/saved-post/SavedPost.tsx
@@ -3,65 +3,15 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  Text,
-  TouchableOpacity,
-  ListRenderItem,
-  Image,
-  ImageSourcePropType,
-} from 'react-native';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import styles from './styles/SavedPost.style';
+import {View, Text} from 'react-native';
+// import styles from './styles/SavedPost.style';
 
-export interface SlideItems {
-  id: string;
-  image: ImageSourcePropType;
-  title: string;
-  date: string;
-}
-
-const Item = ({item}: {item: SlideItems}) => (
-  <TouchableOpacity>
-    <View style={styles.item}>
-      <Image source={item.image} style={styles.image} />
-      <View style={styles.description}>
-        <Text style={styles.title}>{item.title}</Text>
-        <View style={styles.footer}>
-          <MaterialIcons name="update" size={20} color="#999" />
-          <Text style={styles.date}>{item.date}</Text>
-        </View>
-      </View>
-    </View>
-  </TouchableOpacity>
-);
-
-const NewPostsSlider = ({data}: any) => {
-  const renderItem: ListRenderItem<SlideItems> = ({item}) => (
-    <Item item={item} />
-  );
-
+const SavedPost = () => {
   return (
     <View>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>New Posts</Text>
-          <TouchableOpacity>
-            <Text style={styles.headerButton}>Show All</Text>
-          </TouchableOpacity>
-        </View>
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        />
-      </SafeAreaView>
+      <Text>Hello</Text>
     </View>
   );
 };
 
-export default NewPostsSlider;
+export default SavedPost;
--- src/screens/saved/Saved.tsx
+++ src/screens/saved/Saved.tsx
@@ -5,11 +5,13 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Layout from '../../containers/layout/Layout';
+import SavedPost from '../../components/saved-post/SavedPost';
 
 const Saved = ({navigation}: any) => {
   return (
     <Layout navigation={navigation}>
       <Text>This is Saved page</Text>
+      <SavedPost />
     </Layout>
   );
 };
