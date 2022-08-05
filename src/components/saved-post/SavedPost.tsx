365db2409580724b613c1200a5064eff35c03439
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 10:37:47 +0430

    Styling saved post card

--- src/components/saved-post/SavedPost.tsx
+++ src/components/saved-post/SavedPost.tsx
@@ -3,14 +3,18 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
-// import styles from './styles/SavedPost.style';
+import {View, Text, Image, ScrollView} from 'react-native';
+import Picture from './../../assets/posts/1.png';
+import styles from './styles/SavedPost.style';
 
 const SavedPost = () => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image source={Picture} style={styles.image} />
+        <Text>Hello</Text>
+      </View>
+    </ScrollView>
   );
 };
 
--- src/components/saved-post/styles/SavedPost.style.ts
+++ src/components/saved-post/styles/SavedPost.style.ts
@@ -2,8 +2,19 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 24,
-    marginLeft: 16,
+    backgroundColor: '#fff',
+    borderRadius: 24,
+    display: 'flex',
+    flexDirection: 'row',
+    // paddingVertical: 8,
+    // paddingHorizontal: 16,
+    width: '90%',
+    marginLeft: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 24,
   },
 });
 
