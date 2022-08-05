f9f42f635de5ce4353e4b413ccd5e15b38ca33ce
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 11:58:03 +0430

    Change picture of saved data

--- src/utils/saved-posts/SavedPostData.ts
+++ src/utils/saved-posts/SavedPostData.ts
@@ -1,4 +1,11 @@
 import Post1 from './../../assets/posts/1.png';
+import Post2 from './../../assets/posts/2.png';
+import Post3 from './../../assets/posts/3.png';
+import Post4 from './../../assets/posts/4.png';
+import Post5 from './../../assets/posts/5.png';
+import Post6 from './../../assets/posts/6.png';
+import Post7 from './../../assets/posts/7.png';
+import Post8 from './../../assets/posts/8.png';
 
 const SavedPostData = [
   {
@@ -9,43 +16,43 @@ const SavedPostData = [
   },
   {
     id: 2,
-    image: Post1,
+    image: Post2,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
   {
     id: 3,
-    image: Post1,
+    image: Post3,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
   {
     id: 4,
-    image: Post1,
+    image: Post4,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
   {
     id: 5,
-    image: Post1,
+    image: Post5,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
   {
     id: 6,
-    image: Post1,
+    image: Post6,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
   {
     id: 7,
-    image: Post1,
+    image: Post7,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
   {
     id: 8,
-    image: Post1,
+    image: Post8,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
@@ -57,7 +64,7 @@ const SavedPostData = [
   },
   {
     id: 10,
-    image: Post1,
+    image: Post2,
     title: 'Tracking the States Where Abortion Is Now Banned',
     tags: 'Sport, Fitness, Bodybuilder',
   },
