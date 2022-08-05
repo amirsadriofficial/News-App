cde0ab4c78a94d0c354b48270708036a52c1f9e9
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 10:24:42 +0430

    Implement header of saved svreen

--- src/screens/saved/Saved.tsx
+++ src/screens/saved/Saved.tsx
@@ -3,15 +3,24 @@
  */
 
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableOpacity, View, SafeAreaView} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 import Layout from '../../containers/layout/Layout';
 import SavedPost from '../../components/saved-post/SavedPost';
+import styles from './styles/Saved.style';
 
 const Saved = ({navigation}: any) => {
   return (
     <Layout navigation={navigation}>
-      <Text>This is Saved page</Text>
-      <SavedPost />
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.title}>Saved</Text>
+          <TouchableOpacity>
+            <Entypo name="plus" size={30} color="#000" />
+          </TouchableOpacity>
+        </View>
+        <SavedPost />
+      </SafeAreaView>
     </Layout>
   );
 };
--- src/screens/saved/styles/Saved.style.ts
+++ src/screens/saved/styles/Saved.style.ts
@@ -2,8 +2,19 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 24,
-    marginLeft: 16,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginBottom: 16,
+    borderBottomColor: '#898F9C',
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 32,
+    color: '#282828',
   },
 });
 
