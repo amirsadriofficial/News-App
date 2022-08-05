2e3b8696145a974dec01e5bdef3b13e13071767b
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-05 10:56:00 +0430

    Change saved post data

--- src/utils/saved-posts/SavedPostData.ts
+++ src/utils/saved-posts/SavedPostData.ts
@@ -4,52 +4,52 @@ const SavedPostData = [
   {
     id: 1,
     image: Post1,
-    title: 'one Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 2,
     image: Post1,
-    title: 'two Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 3,
     image: Post1,
-    title: 'three Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 4,
     image: Post1,
-    title: 'four Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 5,
     image: Post1,
-    title: 'five Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 6,
     image: Post1,
-    title: 'six Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 7,
     image: Post1,
-    title: 'seven Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 8,
     image: Post1,
-    title: 'eight Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 9,
     image: Post1,
-    title: 'nine Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
   {
     id: 10,
     image: Post1,
-    title: 'ten Post',
+    title: 'Tracking the States Where Abortion Is Now Banned',
   },
 ];
 
