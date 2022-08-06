e12b76ab59da810d960c8f5264c9c64e58c2560b
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-06 11:49:59 +0430

    Create component for footer item

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -2,7 +2,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {Children} from 'react';
 import {SafeAreaView, View, Text, TouchableOpacity} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 import Feather from 'react-native-vector-icons/Feather';
@@ -11,11 +11,31 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import styles from './styles/Footer.style';
 
+type ItemProps = {
+  navigation: any;
+  title: string;
+  children: React.ReactNode;
+};
+
+const Item = ({navigation, title, children}: ItemProps) => {
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate(title)}>
+      <View style={styles.items}>
+        {children}
+        <Text>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
 const Footer = ({navigation}: any) => {
   const route = useRoute();
 
   return (
     <SafeAreaView style={styles.footer}>
+      <Item navigation={navigation} title="Home">
+        <Octicons name="home" size={30} color="#000" />
+      </Item>
       <TouchableOpacity onPress={() => navigation.navigate('Home')}>
         <View style={styles.items}>
           <Octicons name="home" size={30} color="#000" />
