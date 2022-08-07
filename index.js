769b2ffcdf94a42bd47cca6e49947d729ff01517
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-07 13:12:43 +0430

    Install axios

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ import 'react-native-gesture-handler';
  * @format
  */
 
+// import React from 'react';
 import {AppRegistry} from 'react-native';
+// import {Provider} from 'react-redux';
 import App from './src/App';
 import {name as appName} from './app.json';
+// import store from './src/redux/store';
+
+// const RNRedux = () => (
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
 
 AppRegistry.registerComponent(appName, () => App);
