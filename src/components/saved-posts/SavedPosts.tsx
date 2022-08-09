a72b184bd19676a4bba7c57c2ca3898f0176e2c5
Amir Sadri <amirsadriofficial@gmail.com>
2022-08-09 23:04:58 +0430

    Create and styling saved post component

---
+++ src/components/saved-posts/SavedPosts.tsx
@@ -0,0 +1,60 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+  SafeAreaView,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import Octicons from 'react-native-vector-icons/Octicons';
+import styles from './styles/SavedPosts.style';
+import SavedPostData from '../../utils/saved-posts/SavedPostData';
+
+const SavedPosts = ({navigation}: any) => {
+  return (
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+        {SavedPostData.map(post => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('Single Post', {
+                id: post.id,
+              })
+            }>
+            <View style={styles.postContainer}>
+              <Image
+                source={{
+                  uri: post.image,
+                }}
+                style={styles.image}
+              />
+              <View style={styles.descriptionContainer}>
+                <View style={styles.textContainer}>
+                  <Text style={styles.title}>{post.title}</Text>
+                  <View style={styles.tagsContainer}>
+                    <Octicons name="tag" size={15} color="#999" />
+                    <Text style={styles.tags}>{post.tags}</Text>
+                  </View>
+                </View>
+                <Feather
+                  name="more-vertical"
+                  size={25}
+                  color="#000"
+                  style={styles.moreIcon}
+                />
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+export default SavedPosts;
---
+++ src/components/saved-posts/styles/SavedPosts.style.ts
@@ -0,0 +1,53 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 16,
+    marginBottom: 150,
+  },
+  postContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 24,
+    display: 'flex',
+    flexDirection: 'row',
+    width: '90%',
+    marginLeft: 20,
+    marginBottom: 12,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 24,
+  },
+  descriptionContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  textContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginHorizontal: 8,
+  },
+  title: {
+    fontSize: 17,
+    width: 202,
+    height: 65,
+    color: '#282828',
+    marginBottom: 8,
+  },
+  tagsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  tags: {
+    fontSize: 12,
+    marginLeft: 4,
+  },
+  moreIcon: {
+    marginTop: 4,
+  },
+});
+
+export default styles;
